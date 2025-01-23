06f92a27b515612c774c2d1273aaac290a19ddcf
Piet Vinnbruck <pvinnbru@uni-muenster.de>
2025-01-23 18:00:38 +0100

    app.run wurde zweimal ausgeführt. Reloader kostet speicher

--- backend/run.py
+++ backend/run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure tables are created
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
